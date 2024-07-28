75b2a2bf8d3115f0792ff9db47a5d88b83672177
Alfonso Pruneda <alfonso.pru@outlook.com>
2024-07-27 23:32:17 -0600

    Update CountryCard.jsx

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -28,6 +28,7 @@ export default function CountryCard({ flag, name, population, region, capital })
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             variants={itemVariants}
+            tabIndex="-1"
         >
             <img src={flag} alt={`flag of ${name}`} />
             <div className="country-card-text">
