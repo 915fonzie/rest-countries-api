3352c7f91a0648bccc9c1d0880d557e24845d8b3
Alfonso Pruneda <alfonso.pru@outlook.com>
2024-07-27 23:48:08 -0600

    small changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ export default function NavBar() {
     const {theme, toggleTheme} = useContext(ThemeContext)
     return (
         <header className={theme}>
-            <nav>
+            <nav id="main-nav">
                 <Link to="/">Where in the world?</Link>
                 <button onClick={toggleTheme}>
                     {theme === "light-theme" ? <img src={moon} style={{width: "20px"}} alt="moon icon"/> : <img src={sun} style={{width: "20px"}} alt="sun icon"/>}
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './css/index.css'
 import Layout from './components/Layout.jsx'
 import ErrorPage from './pages/ErrorPage.jsx'
 import Homepage, { loader as homeLoader } from './pages/Homepage.jsx'
 import CountryDetails, {loader as detailsLoader} from './pages/CountryDetails.jsx'
+import './css/index.css'
 
 const queryClient = new QueryClient({
   defaultOptions: {
